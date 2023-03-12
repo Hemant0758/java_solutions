5e8b3f80fee3cd78229ac40b93164f9ac3d1bbc0
Hemant0758 <hemant2004ahirwar31@gmail.com>
2023-03-12 23:37:10 +0530

    Chef's son wants to go on a roller coaster ride. The height of Chef's son is � X inches while the minimum height required to go on the ride is � H inches. Determine whether he can go on the ride or not.

---
+++ Minhight.java
@@ -0,0 +1,32 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Minhight
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		while(T-->0){
+		    
+		    int X=sc.nextInt();
+		    int H=sc.nextInt();
+		    
+		    if(X>=H){
+		        System.out.println("YES");
+		        
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+		
+		
+		
+		// your code goes here
+	}
+}
